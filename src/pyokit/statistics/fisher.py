44a238cdd6eda9984b657ccdeb73aaa7610d8911
pjuren <philip.uren@gmail.com>
2014-06-22 19:49:57 -0700

    updates to documentation in statistics modules

--- src/pyokit/statistics/fisher.py
+++ src/pyokit/statistics/fisher.py
@@ -1,39 +1,27 @@
 #!/usr/bin/python
 
 """
-  Date of Creation: 3rd Apr 2010    
-                       
-  Description:   Classes and functions for manipulating BED files
+  Date of Creation: 7th Apr 2012
+  Description:      Python wrapper for Fisher's exact test in R
 
-  Copyright (C) 2010  
+  Copyright (C) 2012-2014
   University of Southern California,
-  Philip J. Uren,
-  Jin H. Park,
-  Andrew D. Smith
-  
-  Authors: Philip J. Uren, Jin H. Park, Andrew D. Smith
-  
+  Philip J. Uren
+
+  Authors: Philip J. Uren
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  
-  --------------------
-  
-  Known Bugs:   None
-  
-  Revision 
-  History:      None
-  
-  TODO:         None
 """
 
 import sys, os, unittest, copy
@@ -51,20 +39,20 @@ def fisherExactTest(a, b, c, d, alternative="two.sided"):
   r("m=matrix(c("+str(a)+","+str(b)+","+str(c)+","+str(d)+"), nrow=2)")
   r("res=fisher.test(m, alternative=\"" + alternative +"\")")
   return r("res$p.value")[0], r("res$estimate")[0]
-  
-  
+
+
 class FisherTests(unittest.TestCase) :
   """
-    @summary: 
+    @summary:
   """
-  
+
   def testFisherExact(self):
-    pval, ratio = fisherExactTest(857,   310487, 
-                                  43058, 28058896, 
+    pval, ratio = fisherExactTest(857,   310487,
+                                  43058, 28058896,
                                   alternative="greater")
     self.assertAlmostEqual(pval, 1.2056e-54)
     self.assertAlmostEqual(ratio, 1.79862, 5)
-    
+
 
 if __name__ == "__main__":
     unittest.main(argv = [sys.argv[0]])
--- src/pyokit/statistics/probability.py
+++ src/pyokit/statistics/probability.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python
 
-""" 
-  Date of Creation: 24th November 2010     
-                      
-  Description:        Classes and functions for generating and selecting
-                      items based on probabilities
+"""
+  Date of Creation: 24th November 2010
+  Description:      Classes and functions for generating and selecting
+                    items based on probabilities
 
-  Copyright (C) 2010  
-  University of Southern California,
-  Philip J. Uren,
-  Andrew D. Smith
-  
+  Copyright (C) 2010-2014
+  Philip J. Uren
+
   Authors: Philip J. Uren
-  
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  
-  --------------------
-  
-  Known Bugs:    None
-  
-  Revision 
-  History:       None 
-  
-  
-  TODO:          None 
 """
 
 import random, unittest, sys
@@ -42,12 +29,12 @@ from datastruct.intervalTree import IntervalTree
 
 class WeightedRandom:
   """
-    Given a list of objects and a weight for each object, this class allows
-    the random choice of an object (with replacement) such that
-    the propbability of getting an object is proportional to it weight 
-    relative to the other objects.
+    @summary Given a list of objects and a weight for each object, this class
+             allows the random choice of an object (with replacement) such that
+             the propbability of getting an object is proportional to it weight
+             relative to the other objects.
   """
-  
+
   class Interval :
     def __init__(self, start, end, obj):
       self.start = start
@@ -55,14 +42,14 @@ class WeightedRandom:
       self.obj = obj
     def __str__(self):
       return str(self.start) + " to " + str(self.end) + " = " + str(self.obj)
-  
+
   def __init__(self, candidates, weights):
     if len(candidates) != len(weights) :
       raise ProbabilityError("number of weights doesn't equal number of " +\
                              "objects")
     self._intervalTree = self._buildTree(weights, candidates)
     self._maxVal = sum(weights)
-    
+
   def _buildTree(self, weights, candidates):
     """
       @summary: build interval tree from cumulative weights
@@ -77,7 +64,7 @@ class WeightedRandom:
       intervals.append(WeightedRandom.Interval(start, end, obj))
       total = total + weight
     return IntervalTree(intervals)
-    
+
   def choose(self):
     rnd = random.uniform(0, self._maxVal)
     objs = self._intervalTree.intersectingPoint(rnd)
@@ -86,7 +73,7 @@ class WeightedRandom:
                              " objects in same cumulative probability range!")
     return objs[0].obj
 
-  
+
 def generateProbabilities(num):
   """
     @summary: generate <num> probabilities such that for each probability x
@@ -97,7 +84,7 @@ def generateProbabilities(num):
   rnds.sort()
   vals = []
   p = 0
-  for n in rnds : 
+  for n in rnds :
     vals.append(n - p)
     p = n
   vals.append(1 - p)
@@ -106,27 +93,27 @@ def generateProbabilities(num):
 
 class ProbabilityTests(unittest.TestCase):
   """
-    @summary: Unit tests for probability 
+    @summary: Unit tests for probability
   """
-    
+
   def setUp(self):
     pass
-  
+
   def testWeightedChoice(self):
     numIter = 100000
     precision = 2
-     
+
     items = ["a","b","c"]
     weights = [0.1, 0.3, 0.6]
     counts = {"a":0, "b":0, "c":0}
     r = WeightedRandom(items, weights)
     for i in range(numIter) :
       counts[r.choose()] += 1
-     
+
     self.assertAlmostEqual(counts["a"] / float(numIter), 0.1, precision)
     self.assertAlmostEqual(counts["b"] / float(numIter), 0.3, precision)
     self.assertAlmostEqual(counts["c"] / float(numIter), 0.6, precision)
-    
+
   def testGenerateProbabilities(self):
     numIter = 1000
     maxProbs = 100
@@ -134,10 +121,10 @@ class ProbabilityTests(unittest.TestCase):
       numProbs = int(random.random() * maxProbs)
       probs = generateProbabilities(numProbs)
       self.assertTrue(sum(probs) == 1)
-      for prob in probs : 
+      for prob in probs :
         self.assertTrue(prob >= 0)
         self.assertTrue(prob <= 1)
 
-  
+
 if __name__ == "__main__":
     unittest.main(argv = [sys.argv[0]])
