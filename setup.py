ab6b980f99af24e5b80aea1aab1653472c9d0ce6
pjuren <philip.uren@gmail.com>
2014-06-25 20:29:16 -0700

    updating setup.py with new version info

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 import sys, os
 
-# in case the user doesn't have setup tools, or the right version
-import ez_setup
-ez_setup.use_setuptools()
-
 # should be able to safely do this now.
 from setuptools import setup, find_packages
 
 setup(name='pyokit',
-      version='0.1',
+      version='0.1.1',
       packages = find_packages('src'),  # include all packages under src
 			package_dir = {'':'src'},   # ell distutils packages are under src
 			description = 'A library of python functions and classes to assist in processing high-throughput biological data',
@@ -16,6 +12,7 @@ setup(name='pyokit',
 			author_email = 'philip.uren@gmail.com',
 			url = 'https://github.com/pjuren/pyokit', 
 			download_url = 'https://github.com/pjuren/pyokit/tarball/pyokit_0.1.1', 
+			license='GPL3',
 			keywords = [],
 			classifiers = [],
 )
