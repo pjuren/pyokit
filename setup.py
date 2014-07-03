868798a92f1745a36c972a90062c1c63e20ddbc7
Philip J. Uren <philip.uren@gmail.com>
2014-07-03 15:43:34 -0700

    Bump version: 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import sys, os
 from setuptools import setup, find_packages
 
 setup(name='pyokit',
-      version='0.1.1',
+      version='0.2.0',
       packages = find_packages('src'),  # include all packages under src
 			package_dir = {'':'src'},   # ell distutils packages are under src
 			description = 'A library of python functions and classes to assist in processing high-throughput biological data',
 			author = 'Philip J. Uren',
 			author_email = 'philip.uren@gmail.com',
 			url = 'https://github.com/pjuren/pyokit', 
-			download_url = 'https://github.com/pjuren/pyokit/tarball/pyokit_0.1.1', 
+			download_url = 'https://github.com/pjuren/pyokit/tarball/pyokit_0.2.0', 
 			license='LGPL2',
 			keywords = [],
 			classifiers = [],
