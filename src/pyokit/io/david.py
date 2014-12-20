19abf64b471bdfff1b78e191cd846733efd3ff31
Philip J. Uren <philip.uren@gmail.com>
2014-12-19 17:16:47 -0800

    adding stubs for procesing DAVID output

---
+++ src/pyokit/io/david.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+"""
+  Date of Creation: 19th Dec 2014
+  Description:      Functions for loading DAVID gene ontology results
+
+  Copyright (C) 2010-2014
+  Philip J. Uren,
+
+  Authors: Philip J. Uren
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+def david_results_iterator(fn, verbose=False):
+  """
+  Iterate over a DAVID result set and yeild GeneOntologyTerm objects
+  representing each of the terms reported.
+
+  :param fn: ...?
+  :param verbose: ...?
+  """
+  pass
+
+
+def david_results_load_file(fn, verbose=False):
+  """
+  Load a set of DAVID gene ontology results as a list of GeneOntologyTerm
+  objects
+
+  :param fn:
+  :param verbose:
+  """
+  pass
