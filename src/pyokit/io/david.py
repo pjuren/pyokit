90dc3e281cd5865bb6b9998a43fe6006f22838af
Philip J. Uren <philip.uren@gmail.com>
2014-12-22 11:56:47 -0800

    filling in stubs for parsing DAVID result files

--- src/pyokit/io/david.py
+++ src/pyokit/io/david.py
@@ -23,17 +23,76 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+# pyokit imports
+from pyokit.datastruct.geneOntology import GeneOntologyEnrichmentResult
+
+###############################################################################
+#                           MODULE-LEVEL CONSTANTS                            #
+###############################################################################
+
+NUM_FIELDS_IN_DAVID_RECORD = 13
+
+
+###############################################################################
+#                                  ITERATORS                                  #
+###############################################################################
 
 def david_results_iterator(fn, verbose=False):
   """
   Iterate over a DAVID result set and yeild GeneOntologyTerm objects
-  representing each of the terms reported.
+  representing each of the terms reported. The expected format for a DAVID
+  result file is tab-seperated format. The following fields should be present:
 
-  :param fn: ...?
-  :param verbose: ...?
+       Field            -- Type   -- Example
+  -----------------------------------------------------------------------------
+  (1)  Category         -- string -- GOTERM_BP_FAT
+  (2)  Term             -- string -- GO:0046907~intracellular transport
+  (3)  Count            -- int    -- 43
+  (4)  Percent          -- float  -- 11.345646437994723
+  (5)  PValue           -- float  -- 1.3232857694449546E-9
+  (6)  Genes            -- string -- ARSB, KPNA6, GNAS,
+  (7)  List Total       -- int    -- 310
+  (8)  Pop Hits	        -- int    -- 657
+  (9)  Pop Total	      -- int    -- 13528
+  (10) Fold Enrichment	-- float  -- 2.8561103746256196
+  (11) Bonferroni	      -- float  -- 2.6293654579179204E-6
+  (12) Benjamini	      -- float  -- 2.6293654579179204E-6
+  (13) FDR              -- float  -- 2.2734203852792234E-6
+
+  The first line is a header giving the field names -- this is ignored though,
+  and we expect them in the order given above.
+
+  Most of the fields are ignored at present; we take fields 1,2, and 13 (as the
+  signficiant/p-value). When parsing the term field, we try to extract a term
+  ID by splitting on tilde.
+
+  :param fn: the file to parse
+  :param verbose: if True, output progress to stderr.
   """
-  pass
+  for line in open(fn):
+    line = line.strip()
+    parts = line.split("\t")
+    if len(parts) != NUM_FIELDS_IN_DAVID_RECORD:
+      raise IOError("failed to parse " + fn + " as DAVID result file. "
+                    + "Expected " + str(NUM_FIELDS_IN_DAVID_RECORD) + " "
+                    + "tab-separated fields, but found "
+                    + str(len(parts)) + " instead")
+    n_parts = parts[2].split("~")
+    name = n_parts[-1].strip()
+    identifier = n_parts[0] if len(n_parts) > 1 else None
+    catagory = parts[1].strip()
+    try:
+      p_val = float(parts[13])
+    except ValueError:
+      raise IOError("Failed to parse " + fn + " as DAVID result file. "
+                    + "Expected field 13 to contain floating point number "
+                    + "(FDR), found this instead: " + str(parts[13]))
+    yield GeneOntologyEnrichmentResult(name, p_val, identifier, catagory)
 
+
+###############################################################################
+#                           BULK LOADING FUNCTIONS                            #
+###############################################################################
 
 def david_results_load_file(fn, verbose=False):
   """
@@ -43,4 +102,4 @@ def david_results_load_file(fn, verbose=False):
   :param fn:
   :param verbose:
   """
-  pass
+  return [x for x in david_results_iterator(fn, verbose)]
