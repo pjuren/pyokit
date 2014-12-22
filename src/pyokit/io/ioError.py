e41619d6c3a67bf5a2d2f0dca4475e2a2359a9af
Philip J. Uren <philip.uren@gmail.com>
2014-12-22 11:12:28 -0800

    adding general exception for IO errors

---
+++ src/pyokit/io/ioError.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+"""
+  Date of Creation: 22nd Dec 2014
+
+  Description:   Generic IO error exception classes
+
+  Copyright (C) 2010-2014
+  Philip J. Uren,
+
+  Authors: Philip J. Uren
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+###############################################################################
+#                            IO EXCEPTION CLASSES                             #
+###############################################################################
+
+class IOError(Exception):
+  """
+  Generic IO error class.
+  """
+  def __init__(self, msg):
+    self.value = msg
+
+  def __str__(self):
+    return repr(self.value)
