0aaa8a042dc4f8201aa190d165afbab47f733d34
Philip J. Uren <philip.uren@gmail.com>
2015-06-19 17:00:55 -0700

    fixed some style problems in CLI module

--- src/pyokit/interface/cli.py
+++ src/pyokit/interface/cli.py
@@ -1,45 +1,59 @@
 #!/usr/bin/python
 
 """
-  Date of Creation: 20th April 2010
-  Description:      Command line parsing module, extends getopt functionality
+Date of Creation: 20th April 2010.
 
-  Copyright (C) 2010-2014
-  Philip J. Uren
+Description:      Command line parsing module, extends getopt functionality
 
-  Authors: Philip J. Uren
+Copyright (C) 2010-2014
+Philip J. Uren
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+Authors: Philip J. Uren
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import sys, copy, getopt
+import sys
+import copy
+import getopt
+
+###############################################################################
+#                                 CONSTANTS                                   #
+###############################################################################
 
 DEFAULT_TYPE = str
 
+
+###############################################################################
+#                            EXCEPTION HANDLING                               #
+###############################################################################
+
 class InterfaceException(Exception):
-  """
-  Exception class representing errors that might occur when parsing
-  command lines
-  """
+
+  """Exception class for errors that might occur when parsing command lines."""
+
   def __init__(self, msg):
+    """constructor for InterfaceException."""
     self.value = msg
+
   def __str__(self):
+    """get string representation of this error."""
     return repr(self.value)
 
 
-class Option :
+class Option(object):
+
   """
   Represents an option for a command line interface.
 
@@ -53,12 +67,11 @@ class Option :
   :param special: if True, disregard requirements for other options when
                   this option is set. Useful for things like help options.
   """
-  def __init__(self, short, long, description, argName = None,
-               default = None, required = False, type = DEFAULT_TYPE,
-               special = False):
-    """
-    Constructor for Option class.
-    """
+
+  def __init__(self, short, long, description, argName=None,
+               default=None, required=False, type=DEFAULT_TYPE,
+               special=False):
+    """Constructor for Option class. See class docstring for param defs."""
     self.short = short
     self.long = long
     self.description = description
@@ -74,41 +87,43 @@ class Option :
 
     # if a default value is given, we set it to that on creation
     # (note that we don't count this as 'setting' the option)
-    if self.default != None :
+    if self.default is not None:
       self.value = self.type(default)
 
     # short option names should only be 1 char
-    if len(self.short) != 1 :
+    if len(self.short) != 1:
       raise InterfaceException("short option names must be 1 character long")
 
     # doens't make sense to tell us the type but not the name of the arg
-    if self.type != DEFAULT_TYPE and argName == None :
+    if self.type != DEFAULT_TYPE and argName is None:
       raise InterfaceException("Got argument type but no name")
 
   def isSet(self):
     """
     Check whether this option has been set by the user or not.
+
     :return: True if this option has been set by the user, else false.
     """
     return self.set
 
   def isRequired(self):
     """
-    Check whether this is a required option (i.e. whether parsing a command
-    line should fail when it is missing).
+    Check whether this is a required option.
+
+    Parsing command lines should fail when required options are missing
+
     :return: True if this option is required, else false.
     """
     return self.required
 
   def __str__(self):
-    """
-    Get a string representation of this option.
-    """
-    return  "-" + self.short + ", --" + self.long + " : " +\
-            self.description.replace("\t","\\t") + " (required? " +\
-            str(self.required) + ")"
+    """Get a string representation of this option."""
+    return "-" + self.short + ", --" + self.long + " : " +\
+           self.description.replace("\t", "\\t") + " (required? " +\
+           str(self.required) + ")"
 
-class CLI :
+class CLI(object):
+
   """
   Represents a command line interface for a program, including the number
   of arguemnts and the options the program accepts. Provides methods for parsing
@@ -122,9 +137,11 @@ class CLI :
 
   def __init__(self, progName, shortDesc, longDesc):
     """
-    CLI constructor -- builds a CLI object that defines an interface, but has
-    not yet been populated with user-supplied data; see class-level
-    documentation for parameter descriptions.
+    CLI constructor.
+
+    Builds a CLI object that defines an interface, but has not yet been
+    populated with user-supplied data; see class-level documentation for
+    parameter descriptions.
     """
     self.programName = progName
     self.longDescription = longDesc
@@ -135,34 +152,34 @@ class CLI :
     self.minArgs = None
     self.maxArgs = None
 
-
   def usage(self):
-    """
-    Print the usage description for this program. Output goes to standard out.
-    """
+    """Print usage description for this program. Output is to standard out."""
     print self.programName + " [options]",
-    if self.maxArgs == -1 or self.maxArgs - self.minArgs > 1 : print "[files..]"
-    elif self.maxArgs - self.minArgs == 1: print "[file]"
+    if self.maxArgs == -1 or self.maxArgs - self.minArgs > 1:
+      print "[files..]"
+    elif self.maxArgs - self.minArgs == 1:
+      print "[file]"
     print
 
     print self.shortDescription
 
     print "Options:"
-    for o in self.options :
+    for o in self.options:
       print "\t", ("-" + o.short + ", --" + o.long).ljust(15), ":",
-      print o.description.replace("\t","\\t").ljust(80)
+      print o.description.replace("\t", "\\t").ljust(80)
 
   def parseCommandLine(self, line):
     """
-    Parse the given command line and populate this CLI object with the values
-    found. If the command line doesn't conform to the specification defined
-    by this CLI object, this function prints a message to stdout indicating what
-    was wrong, prints the program usage instructions and then exists the program
+    Parse the given command line and populate self with the values found.
+
+    If the command line doesn't conform to the specification defined
+    by this CLI object, this function prints a message to stdout indicating
+    what was wrong, prints the program usage instructions and then exists
+    the program
 
     :param line: list of tokens from the command line. Should have had program
                  name removed - generally one would do this: sys.argv[1:]
     """
-
     # separate arguments and options
     try:
       optlist, args = getopt.getopt(line, self._optlist(), self._longoptl())
@@ -172,37 +189,38 @@ class CLI :
       sys.exit(2)
 
     # parse options
-    for opt, arg in optlist :
+    for opt, arg in optlist:
       opt = opt.lstrip("-")
       found = False
-      for option in self.options :
-        if option.short == opt or option.long == opt :
+      for option in self.options:
+        if option.short == opt or option.long == opt:
           option.value = option.type(arg)
           option.set = True
           found = True
-      if not found :
+      if not found:
         sys.stderr.write("unknown option: " + opt)
         self.usage()
         sys.exit(2)
 
     # check for special options
     specialOptions = False
-    for opt in self.options :
-      if opt.special and opt.isSet() : specialOptions = True
+    for opt in self.options:
+      if opt.special and opt.isSet():
+        specialOptions = True
 
     # parse arguments
-    if len(args) < self.minArgs and not specialOptions :
-      err = str(len(args)) + " input arguments found, but required at least " +\
-            str(self.minArgs) + " arguments"
+    if len(args) < self.minArgs and not specialOptions:
+      err = (str(len(args)) + " input arguments found, but required at " +
+             "least " + str(self.minArgs) + " arguments")
       sys.stderr.write(err)
       sys.stderr.write("\n\n")
       self.usage()
       sys.exit(2)
-    else :
+    else:
       self.args = args
 
     # check for required options
-    for option in self.options :
+    for option in self.options:
       if option.isRequired() and not option.isSet() and not specialOptions:
         err = "required option \'" + str(option) + "\' is missing"
         sys.stderr.write(err)
@@ -212,20 +230,20 @@ class CLI :
 
   def getOption(self, name):
     """
-    Get the the Option object associated with the given name
+    Get the the Option object associated with the given name.
 
     :param name: the name of the option to retrieve; can be short or long name.
     :raise InterfaceException: if the named option doesn't exist.
     """
     name = name.strip()
-    for o in self.options :
-      if o.short == name or o.long == name :
+    for o in self.options:
+      if o.short == name or o.long == name:
         return o
     raise InterfaceException("No such option: " + name)
 
   def getValue(self, name):
     """
-    Get the value of the option matching the name given
+    Get the value of the option matching the name given.
 
     :param name: the name of the option to retrieve; can be short or long name.
     :raise InterfaceException: if the named option doesn't exist.
@@ -234,38 +252,39 @@ class CLI :
 
   def hasOption(self, name):
     """
-    Check whether this CLI has an option with a given name
+    Check whether this CLI has an option with a given name.
 
     :param name: the name of the option to check; can be short or long name.
     :return: true if an option exists in the UI for the given name
     """
     name = name.strip()
-    for o in self.options :
-      if o.short == name or o.long == name :
+    for o in self.options:
+      if o.short == name or o.long == name:
         return True
     return False
 
   def addOption(self, o):
     """
-    Add a new Option to this CLI
+    Add a new Option to this CLI.
 
     :param o: the option to add
     :raise InterfaceException: if an option with that name already exists.
     """
-    if self.hasOption(o.short) :
+    if self.hasOption(o.short):
       raise InterfaceException("Failed adding option - already have " + str(o))
     self.options.append(o)
 
   def optionIsSet(self, name):
     """
-    Check whether an option with a given name exists and has been set
+    Check whether an option with a given name exists and has been set.
 
     :param name: the name of the option to check; can be short or long name.
     :return: true if an option matching the given name exists and it has had
              it's value set by the user
     """
     name = name.strip()
-    if not self.hasOption(name) : return False
+    if not self.hasOption(name):
+      return False
     return self.getOption(name).isSet()
 
   def hasArgument(self, num):
@@ -288,38 +307,35 @@ class CLI :
              will be a string.
     :raise InterfaceException: if there are fewer than num + 1 arguments.
     """
-    if not self.hasArgument(num) :
-      raise InterfaceException("Failed to retrieve argument " + str(num) +\
+    if not self.hasArgument(num):
+      raise InterfaceException("Failed to retrieve argument " + str(num) +
                                " -- not enough arguments provided")
     return self.args[num]
 
   def getAllArguments(self):
     """
     Get a list of all the arguments.
+
     :return: a list of all the arguments, each as a string.
     """
     # don't leak a reference to the internal variable args
     return copy.copy(self.args)
 
   def _optlist(self):
-    """
-    Get a string representation of the options in short format.
-    """
+    """Get a string representation of the options in short format."""
     res = ""
-    for o in self.options :
+    for o in self.options:
         res += o.short
-        if o.argName != None :
+        if o.argName is not None:
           res += ":"
     return res
 
   def _longoptl(self):
-    """
-    Get a list of string representations of the options in long format.
-    """
+    """Get a list of string representations of the options in long format."""
     res = []
-    for o in self.options :
+    for o in self.options:
       nm = o.long
-      if o.argName is not None :
+      if o.argName is not None:
         nm += "="
       res.append(nm)
     return res
