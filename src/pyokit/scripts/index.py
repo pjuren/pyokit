e8c8541521eeb7a93f26c241c204f92f53dbcec9
Philip J. Uren <philip.uren@gmail.com>
2015-07-03 15:40:14 -0700

    implemented the index lookup command and re-arranged the UI a bit for the index script.

--- src/pyokit/scripts/index.py
+++ src/pyokit/scripts/index.py
@@ -33,6 +33,7 @@ import functools
 
 # pyokit imports -- general
 from pyokit.interface.cli import CLI, Option
+from pyokit.interface.uiError import PyokitUIError
 from pyokit.io.indexedFile import IndexedFile
 from pyokit.io.indexedFile import IndexError
 
@@ -84,7 +85,7 @@ def index_repeatmasker_alignment_by_id(fh, out_fh, vebrose=False):
 def index_genome_alignment_by_locus(fh, out_fh, verbose=False):
   """Build an index for a genome alig. using coords in ref genome as keys."""
   bound_iter = functools.partial(genome_alignment_iterator,
-                                 reference_species="hg19")
+                                 reference_species="hg19", index_friendly=True)
   hash_func = JustInTimeGenomeAlignmentBlock.build_hash
   idx = IndexedFile(fh, bound_iter, hash_func)
   idx.write_index(out_fh, verbose=verbose)
@@ -108,12 +109,66 @@ def get_indexer_by_file_extension(ext):
 
 
 ###############################################################################
+#                              INDEX LOOKUPS                                  #
+###############################################################################
+
+def lookup_genome_alignment_index(index_fh, indexed_fh, out_fh=sys.stdout,
+                                  key=None, verbose=False):
+  """Load a GA index and its indexed file and extract one or more blocks.
+
+  :param index_fh:   the index file to load. Can be a filename or a
+                     stream-like object.
+  :param indexed_fh: the file that the index was built for,
+  :param key:        A single key, iterable of keys, or None. This key will be
+                     used for lookup. If None, user is prompted to enter keys
+                     interactively.
+  """
+  # load the genome alignment as a JIT object
+  bound_iter = functools.partial(genome_alignment_iterator,
+                                 reference_species="hg19", index_friendly=True)
+  hash_func = JustInTimeGenomeAlignmentBlock.build_hash
+  idx = IndexedFile(record_iterator=bound_iter, record_hash_function=hash_func)
+  idx.read_index(index_fh, indexed_fh)
+
+  if key is None:
+    while key is None or key.strip() != "":
+      sys.stderr.write("[WAITING FOR KEY ENTRY ON STDIN; " +
+                       "END WITH EMPTY LINE]\n")
+      key = raw_input()
+      # we know keys for genome alignments have tabs as delims, so..
+      key = '\t'.join(key.split()).strip()
+      if key != "":
+        out_fh.write(str(idx[key]) + "\n")
+      sys.stderr.write("\n")
+  else:
+    # we know keys for genome alignments have tabs as delims, so..
+    key = '\t'.join(key.split())
+    out_fh.write(str(idx[key]) + "\n")
+
+
+def get_lookup_by_filetype(fileType):
+  """Find the right indexer for a user-specified filetype."""
+  try:
+    if fileType is FileType.genome_alignment:
+      return lookup_genome_alignment_index
+  finally:
+    raise ValueError("Unsupported file type: " + str(fileType))
+
+
+def get_lookup_by_file_extension(ext):
+  """Find right indexer using file extension."""
+  if ext == ".maf" or ext == "maf":
+    return lookup_genome_alignment_index
+  else:
+    raise ValueError("Unsupported file type: " + str(ext))
+
+###############################################################################
 #                              USER INTERFACE                                 #
 ###############################################################################
 
-def getUI(prog_name, args):
+def getUI_build_index(prog_name, args):
   """
-  build and return a UI object for this script.
+  build and return a UI object for the 'build' option.
 
   :param args: raw arguments to parse
   """
@@ -132,7 +187,47 @@ def getUI(prog_name, args):
                       "the script will try to guess the file type. " +
                       "Supported file types are: " +
                       ", ".join([f.name for f in FileType]),
-                      default=4, required=False, type=int))
+                      required=False, type=str))
+  ui.addOption(Option(short="v", long="verbose",
+                      description="output additional messages to stderr " +
+                                  "about run (default: " +
+                                  str(DEFAULT_VERBOSITY) + ")",
+                      default=DEFAULT_VERBOSITY, required=False))
+  ui.addOption(Option(short="h", long="help",
+                      description="show this help message ", special=True))
+  ui.addOption(Option(short="u", long="test",
+                      description="run unit tests ", special=True))
+
+  ui.parseCommandLine(args)
+  return ui
+
+
+def getUI_lookup_index(prog_name, args):
+  """
+  build and return a UI object for the 'build' option.
+
+  :param args: raw arguments to parse
+  """
+  programName = prog_name
+  long_description = "Build an index for one or more files."
+  short_description = long_description
+
+  ui = CLI(programName, short_description, long_description)
+  ui.minArgs = 2
+  ui.maxArgs = 2
+  ui.addOption(Option(short="o", long="output", argName="filename",
+                      description="output to given file, else stdout",
+                      required=False, type=str))
+  ui.addOption(Option(short="k", long="key", argName="key",
+                      description="the key to lookup; if missing, the " +
+                      "program runs in interactive mode. ",
+                      required=False, type=str))
+  ui.addOption(Option(short="t", long="type", argName="filename",
+                      description="the type of the file. If missing, " +
+                      "the script will try to guess the file type. " +
+                      "Supported file types are: " +
+                      ", ".join([f.name for f in FileType]),
+                      required=False, type=str))
   ui.addOption(Option(short="v", long="verbose",
                       description="output additional messages to stderr " +
                                   "about run (default: " +
@@ -172,7 +267,78 @@ def __get_indexer(in_fns, selected_type=None):
   return indexer
 
 
+def __get_lookup(in_fn, selected_type=None):
+  """Determine which lookup func to use based on inpt files and type option."""
+  lookup_func = None
+  if selected_type is not None:
+    lookup_func = get_lookup_by_filetype(selected_type)
+  else:
+    extension = os.path.splitext(in_fn)[1]
+    lookup_func = get_lookup_by_file_extension(extension)
+  assert(lookup_func is not None)
+  return lookup_func
+
+
+def isHelpString(s):
+  """determine whether an option is a help string."""
+  norm = s.strip().lower()
+  return norm == "help" or norm == "--help" or norm == "-h"
+
+
 def main(args=[], prog_name=sys.argv[0]):
+  """Figure out which of the two options was used (build, get) and dispatch.
+
+  :param args:      the arguments for this script, as a list of string. Should
+                    already have had the script name stripped, so we expect the
+                    first item in this list to specify the mode ('build' an
+                    index or 'get' elements from an indexed file).
+  :param prog_name: the name of the script; taken from command line args by
+                    default, but you can change it if you want.
+  """
+  helpStr = "Build indexes and use them to extract elements from         \n" +\
+            "indexed files on-demand.                                    \n" +\
+            "                                                            \n" +\
+            "------------------------------------------------------------\n" +\
+            "                      BUILDING AN INDEX                     \n" +\
+            "------------------------------------------------------------\n" +\
+            "                                                            \n" +\
+            "To build an index for a file called foo.maf, where $ is     \n" +\
+            "your prompt:                                                \n" +\
+            "                                                            \n" +\
+            "$ pyokit index build -o foo.idx foo.maf                     \n" +\
+            "                                                            \n" +\
+            "for more information about the build command, where $ is    \n" +\
+            "your prompt, run:                                           \n" +\
+            "                                                            \n" +\
+            "$ pyokit index build --help                                 \n" +\
+            "                                                            \n" +\
+            "------------------------------------------------------------\n" +\
+            "                           LOOKUP                           \n" +\
+            "------------------------------------------------------------\n" +\
+            "To run interactive lookup of values using an index , where $\n" +\
+            "is your prompt:                                             \n" +\
+            "                                                            \n" +\
+            "$ pyokit index lookup foo.maf foo.idx                       \n" +\
+            "                                                            \n" +\
+            "for more information about the lookup command, where $ is   \n" +\
+            "your prompt, run:                                           \n" +\
+            "                                                            \n" +\
+            "$ pyokit index lookup --help                                \n" +\
+            "                                                              "
+
+  if len(args) == 0 or (len(args) == 1 and isHelpString(args[0])):
+    sys.stderr.write(helpStr + "\n\n")
+  else:
+    arg_one = args[0].strip().lower()
+    if arg_one == "build":
+      main_build_index(args[1:])
+    elif arg_one == "lookup":
+      main_lookup_index(args[1:])
+    else:
+      raise PyokitUIError("Unknown option " + arg_one)
+
+
+def main_lookup_index(args=[], prog_name=sys.argv[0]):
   """
   main entry point for the index script.
 
@@ -183,7 +349,52 @@ def main(args=[], prog_name=sys.argv[0]):
                     default, but you can change it if you want.
   """
   # get options and arguments
-  ui = getUI(prog_name, args)
+  ui = getUI_lookup_index(prog_name, args)
+
+  # just run unit tests
+  if ui.optionIsSet("test"):
+    unittest.main(argv=[sys.argv[0]])
+    sys.exit()
+
+  # just show help
+  if ui.optionIsSet("help"):
+    ui.usage()
+    sys.exit()
+  verbose = (ui.optionIsSet("verbose") == True) or DEFAULT_VERBOSITY
+
+  # required exactly two args, so this should be fine.
+  in_indexed, in_index = open(ui.getArgument(0)), open(ui.getArgument(1))
+
+  # Get output handle(s). If only one input file, and no output file, output
+  # to standard out.
+  out_fh = sys.stdout
+  if ui.optionIsSet("output"):
+    out_fh = open(ui.getValue("output"), "w")
+
+  # get the lookup key
+  key = None
+  if ui.optionIsSet("key"):
+    key = ui.getValue("key")
+
+  # figure out which indexer to use and then index each file
+  op_val = ui.getValue("type") if ui.optionIsSet("type") else None
+  lookup_func = __get_lookup(ui.getArgument(0), op_val)
+
+  lookup_func(in_index, in_indexed, out_fh, key, verbose)
+
+
+def main_build_index(args=[], prog_name=sys.argv[0]):
+  """
+  main entry point for the index script.
+
+  :param args:      the arguments for this script, as a list of string. Should
+                    already have had the script name stripped. That is, if
+                    there are no args provided, this should be an empty list.
+  :param prog_name: the name of the script; taken from command line args by
+                    default, but you can change it if you want.
+  """
+  # get options and arguments
+  ui = getUI_build_index(prog_name, args)
 
   # just run unit tests
   if ui.optionIsSet("test"):
@@ -253,7 +464,7 @@ class TestIndex(unittest.TestCase):
       raise IOError("No such file")
 
     mock_open.side_effect = open_side_effect
-    main(["-o", "one.idx", "one.maf"])
+    main(["build", "-o", "one.idx", "one.maf"])
     idx_strm.seek(0)
     in_strm.seek(0)
     ga = build_genome_alignment_from_file("one.maf", "hg19", "one.idx")
@@ -261,6 +472,39 @@ class TestIndex(unittest.TestCase):
     b2_res = ga.get_blocks("chr22", 1770, 1780)[0]
     self.assertEqual(b1_res["hg19.chr22"], self.b1_hg19)
     self.assertEqual(b2_res["hg19.chr22"], self.b2_hg19)
+
+  @mock.patch('__builtin__.open')
+  def test_index_lookup(self, mock_open):
+    """Test lookup of specific key using full UI."""
+    in_strm = StringIO.StringIO(self.ga_maf1)
+    idx_strm = StringIO.StringIO()
+    out_strm = StringIO.StringIO()
+
+    # replace open with mock
+    def open_side_effect(*args, **kwargs):
+      if not isinstance(args[0], basestring):
+        raise TypeError()
+      if args[0] == "one.maf":
+        return in_strm
+      elif args[0] == "one.idx":
+        return idx_strm
+      elif args[0] == "out.txt":
+        return out_strm
+      raise IOError("No such file")
+
+    mock_open.side_effect = open_side_effect
+
+    # build and index in idx_strm
+    bound_iter = functools.partial(genome_alignment_iterator,
+                                   reference_species="hg19")
+    hash_func = JustInTimeGenomeAlignmentBlock.build_hash
+    idx = IndexedFile(StringIO.StringIO(self.ga_maf1), bound_iter, hash_func)
+    idx.write_index(idx_strm)
+    idx_strm.seek(0)
+
+    key = "chr22" + "\t" + "1772" + "\t" + "1825"
+    main(["lookup", "-o", "out.txt", "-k", key, "one.maf", "one.idx"])
+    self.assertEqual(str(idx[key]).strip(), str(out_strm.getvalue()).strip())
 
 
 ###############################################################################
