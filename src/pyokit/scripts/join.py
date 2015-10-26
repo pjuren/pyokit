71d5fb60d6663a98c9b6a0960b8c77b5e373de8b
Philip Uren <philip.uren@gmail.com>
2015-10-25 17:06:37 -0700

    switched code for processing unpaired keys in the single-pass algorithm in joinpy over to share the same function call as the version that loads both files at once -- simpler

--- src/pyokit/scripts/join.py
+++ src/pyokit/scripts/join.py
@@ -420,6 +420,31 @@ def load_file(fn_or_strm, key, key_is_field_number, require_unique_key=True,
   return res, h_res
 
 
+def __output_unpaired_vals(d_vals, used_ff_keys, f_f_header, sf_d, s_f_header,
+                           missing_val, out_handler, outfh, delim="\t"):
+  """
+  Use an output handler to output keys that could not be paired.
+
+  Go over the keys in d_vals and for any that were not used (i.e. not in
+  used_ff_keys), build an output line using the values from d_vals,
+  populated the missing columns with missing_val, and output these using the
+  provided output hander.
+  """
+  if missing_val is None:
+    raise MissingValueError("Need missing value to output " +
+                            " unpaired lines")
+  for k in d_vals:
+    if k not in used_ff_keys:
+      f_f_flds = d_vals[k]
+      if s_f_header is not None:
+        s_f_flds = [dict(zip(s_f_header, [missing_val] * len(s_f_header)))]
+      else:
+        s_f_num_cols = len(sf_d[d_vals.keys()[0]][0])
+        s_f_flds = [[missing_val] * s_f_num_cols]
+      out_handler.write_output(outfh, delim, s_f_flds, f_f_flds,
+                               s_f_header, f_f_header)
+
+
 ###############################################################################
 #                               USER INTERFACE                                #
 ###############################################################################
@@ -660,19 +685,8 @@ def process_without_storing(d_vals, s_f_strm, s_f_key, output_type, outfh,
     first_element = False
 
   if output_unpaired:
-    for k in d_vals:
-      if k not in used_d_val_keys:
-        if missing_val is None:
-          raise MissingValueError("Need missing value to output " +
-                                  " unpaired lines")
-        f_f_flds = d_vals[k]
-        if s_f_header is not None:
-          s_f_flds = [dict(zip(s_f_header, [missing_val] * len(s_f_header)))]
-        else:
-          s_f_num_cols = len(parts)
-          s_f_flds = [[missing_val] * s_f_num_cols]
-        out_handler.write_output(outfh, delim, s_f_flds, f_f_flds,
-                                 s_f_header, f_f_header)
+    __output_unpaired_vals(d_vals, used_d_val_keys, f_f_header, s_f_flds,
+                           s_f_header, missing_val, out_handler, outfh, delim)
 
 
 def process_by_storing(d_vals, s_f_strm, s_f_key, output_type, outfh,
@@ -716,25 +730,8 @@ def process_by_storing(d_vals, s_f_strm, s_f_key, output_type, outfh,
                              f_f_header)
 
   if output_unpaired:
-    output_unpaired_vals(d_vals, used_ff_keys, f_f_header, sf_d, s_f_header,
-                         missing_val, out_handler, outfh, delim)
-
-
-def output_unpaired_vals(d_vals, used_ff_keys, f_f_header, sf_d, s_f_header,
-                         missing_val, out_handler, outfh, delim="\t"):
-  if missing_val is None:
-    raise MissingValueError("Need missing value to output " +
-                            " unpaired lines")
-  for k in d_vals:
-    if k not in used_ff_keys:
-      f_f_flds = d_vals[k]
-      if s_f_header is not None:
-        s_f_flds = [dict(zip(s_f_header, [missing_val] * len(s_f_header)))]
-      else:
-        s_f_num_cols = len(sf_d[d_vals.keys()[0]][0])
-        s_f_flds = [[missing_val] * s_f_num_cols]
-      out_handler.write_output(outfh, delim, s_f_flds, f_f_flds,
-                               s_f_header, f_f_header)
+    __output_unpaired_vals(d_vals, used_ff_keys, f_f_header, sf_d, s_f_header,
+                           missing_val, out_handler, outfh, delim)
 
 
 def process(infh1, infh2, outfh, key_one, key_one_is_field_number, key_two,
