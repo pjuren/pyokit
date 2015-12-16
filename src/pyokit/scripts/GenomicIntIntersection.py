3526ed864c4efc3a608cbc4c81a8563fa99a4223
Philip Uren <philip.uren@gmail.com>
2015-12-15 17:25:46 -0800

    adding stubs for the genomic interval intersection and union scripts

---
+++ src/pyokit/scripts/GenomicIntIntersection.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+"""
+  Date of Creation: Dec 2015
+  Description:      Get the intersection of genomic regions in one or more
+                    files
+
+  Copyright (C) 2010-2014
+  Philip J. Uren
+
+  Authors: Philip J. Uren
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
